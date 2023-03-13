4a544d2f160a21d45e62700a20f8c9318db9f2c1
Ryan Coles <ryanc2811@live.co.uk>
2023-03-13 09:58:11 +0000

    Create Employer Page started

--- glpxllamally/src/firebase.js
+++ glpxllamally/src/firebase.js
@@ -15,6 +15,8 @@ import {
   collection,
   where,
   addDoc,
+  doc,
+  getDoc
 } from "firebase/firestore";
 const firebaseConfig = {
 
@@ -39,6 +41,7 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
+
 const googleProvider = new GoogleAuthProvider();
 const signInWithGoogle = async () => {
   try {
@@ -47,11 +50,19 @@ const signInWithGoogle = async () => {
     const q = query(collection(db, "users"), where("uid", "==", user.uid));
     const docs = await getDocs(q);
     if (docs.docs.length === 0) {
+      const privileges=[]
+    const docRef=doc(db,"user_privileges","eh0Ki56hzby4mUcIimEr")
+    const docSnap=await getDoc(docRef)
+    
+    if(docSnap.exists()){
+      privileges.push(docSnap.id);
+    }
       await addDoc(collection(db, "users"), {
         uid: user.uid,
         name: user.displayName,
         authProvider: "google",
         email: user.email,
+        privileges
       });
     }
   } catch (err) {
@@ -71,12 +82,26 @@ const registerWithEmailAndPassword = async (name, email, password) => {
   try {
     const res = await createUserWithEmailAndPassword(auth, email, password);
     const user = res.user;
+    const privileges=[]
+    const docRef=doc(db,"user_privileges","eh0Ki56hzby4mUcIimEr")
+    const docSnap=await getDoc(docRef)
+    
+    if(docSnap.exists()){
+      privileges.push(docSnap.id);
+    }
     await addDoc(collection(db, "users"), {
       uid: user.uid,
       name,
       authProvider: "local",
       email,
+      privileges
     });
+      // const member_data={
+    //   user_id:user.uid
+    // }
+
+    // await addDoc(collection(db,"members"),member_data);
+
   } catch (err) {
     console.error(err);
     alert(err.message);
