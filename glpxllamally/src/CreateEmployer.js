e23b924d518d5bb888718893bfb5b1c4c0aa3b41
Ryan Coles <ryanc2811@live.co.uk>
2023-03-13 11:23:47 +0000

    test

--- glpxllamally/src/CreateEmployer.js
+++ glpxllamally/src/CreateEmployer.js
@@ -4,7 +4,6 @@ import { Link, useNavigate } from "react-router-dom";
 import {
   auth,
   registerWithEmailAndPassword,
-  signInWithGoogle,
 } from "./firebase";
 import "./CreateEmployer.css";
 function CreateEmployer() {
